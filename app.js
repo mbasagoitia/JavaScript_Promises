4575ed28f5cbf44101f2bf0185b87c9262a7c004
basagoitiam <marika.basagoitia@gmail.com>
2022-09-29 11:53:41 -0400

    finished exercise

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
+
+const error = document.querySelector("#error");
+const list = document.querySelector("#list");
+
+
 function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let potentialFail = Math.round(Math.random() * 100) < 10;
+      //I changed the number to < 50 here just so that I could see the fail message more often to check my work
+      let potentialFail = Math.round(Math.random() * 100) < 50;
       if (potentialFail) {
         reject({ success: false, message: "Failed to get list of hobbits." });
       } else {
@@ -14,6 +20,16 @@ function getList() {
     }, 10);
   });
 }
+
+getList().then((result) => {
+  result.forEach((hobbit) => {
+    const li = document.createElement("li");
+    li.textContent = hobbit;
+    list.appendChild(li);
+  })
+}).catch((err) => {
+  error.textContent = err.message;
+})
 
 // TODO: Handle the resolved or rejected states of the promise
 
